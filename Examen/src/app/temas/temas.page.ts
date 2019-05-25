461f4848fec7206d0038d7faa88c615bafe1224e
César Alejandro Montaño Cortés <cesarin_mon@hotmail.com>
2019-05-25 03:24:03 -0500

    Home

--- Examen/src/app/temas/temas.page.ts
+++ Examen/src/app/temas/temas.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-temas',
-  templateUrl: './temas.page.html',
+  templateUrl: './index.page.html',
   styleUrls: ['./temas.page.scss'],
 })
 export class TemasPage implements OnInit {
